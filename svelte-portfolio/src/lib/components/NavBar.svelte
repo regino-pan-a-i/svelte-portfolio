08ca67725cce210baf68758cb267333a9628afe2
Andre Regino <reginopanai@gmail.com>
2025-04-15 23:08:44 -0600

    Createed routes for rest of website

--- svelte-portfolio/src/lib/components/NavBar.svelte
+++ svelte-portfolio/src/lib/components/NavBar.svelte
@@ -7,16 +7,16 @@
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <ul class="flex flex-col h-full space-y-5 font-medium align-middle justify-center gap-5.5">
             <li >
-                <a href="#" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">ABOUT </a>
+                <a href="/about" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">ABOUT </a>
             </li>
             <li>
                 <a href="/projects" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">PROJECTS</a>
             </li>
             <li>
-                <a href="#" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">EXPERIENCE</a>
+                <a href="/experience" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">EXPERIENCE</a>
             </li>
             <li>
-                <a href="#" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">CONTACT</a>
+                <a href="#contact" class="items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">CONTACT</a>
             </li>
         </ul>
     </div>
