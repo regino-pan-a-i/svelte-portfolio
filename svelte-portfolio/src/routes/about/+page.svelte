2ef774dc216dded9b7f040b8d1af87164c38ff29
Andre Regino <reginopanai@gmail.com>
2025-04-19 23:28:58 -0600

    Styled the headings of each page

--- svelte-portfolio/src/routes/about/+page.svelte
+++ svelte-portfolio/src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <div>
-    <h1>About me </h1>
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">About me </h1>
 
 
     <p>I’m a senior Software Engineering student at Brigham Young University–Idaho with a 3.95 GPA, graduating in December 2025. I’m passionate about building meaningful, user-centered software solutions and have a strong foundation in Java, full-stack web development, and mobile app development. Over the past few years, I’ve completed projects ranging from a class planning tool using the MERN stack to a Spring Boot e-commerce platform with real-time cart cleanup using multithreading and MongoDB. I recently earned my AWS Cloud Practitioner certification, expanding my skills into cloud-based deployment and DevOps.
--- svelte-portfolio/src/routes/experience/+page.svelte
+++ svelte-portfolio/src/routes/experience/+page.svelte
@@ -1,4 +1,4 @@
 <div>
-    <h1>Experience </h1>
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white" >Experience </h1>
 
 </div>
