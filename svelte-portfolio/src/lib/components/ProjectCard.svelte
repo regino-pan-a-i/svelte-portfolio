2314662ab8cdcea0d6e412d3112cdf64da000a2c
Andre Regino <reginopanai@gmail.com>
2025-04-21 14:08:56 -0600

    Created project description card and started working on a carousel to show all the projects

---
+++ svelte-portfolio/src/lib/components/ProjectCard.svelte
@@ -0,0 +1,17 @@
+<script>
+
+let {project} = $props()
+
+
+</script>
+
+
+<div class="overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200">
+    <!-- Image -->
+    <figure class="relative">
+      <img src="https://picsum.photos/id/100/800/600" alt="card image" class="aspect-video w-full" />
+      <figcaption class="absolute bottom-0 left-0 w-full p-6 text-white bg-gradient-to-t from-slate-900">
+        <h3 class="text-lg font-medium ">{project.title}</h3>
+      </figcaption>
+    </figure>
+</div>
---
+++ svelte-portfolio/src/lib/components/ProjectCarousel.svelte
@@ -0,0 +1,57 @@
+<script>
+    import Glide from '@glidejs/glide'
+
+    var glide01 = new Glide('.glide-01', {
+        type: 'carousel',
+        focusAt: 'center',
+        perView: 3,
+        autoplay: 3000,
+        animationDuration: 700,
+        gap: 24,
+        classes: {
+            activeNav: '[&>*]:bg-slate-700',
+        },
+        breakpoints: {
+            1024: {
+                perView: 2
+            },
+            640: {
+                perView: 1
+            }
+        },
+    });
+
+    glide01.mount();
+</script>
+
+<div class="relative w-full glide-01">
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.0.2/glide.js"></script> -->
+    <!-- Slides -->
+    <div class="overflow-hidden" data-glide-el="track">
+        <ul class="relative w-full overflow-hidden p-0 whitespace-no-wrap flex flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform]">
+            <li><img src="https://Tailwindmix.b-cdn.net/carousel/carousel-image-01.jpg" class="w-full max-w-full max-h-full m-auto" /></li>
+            <li><img src="https://Tailwindmix.b-cdn.net/carousel/carousel-image-02.jpg" class="w-full max-w-full max-h-full m-auto" /></li>
+            <li><img src="https://Tailwindmix.b-cdn.net/carousel/carousel-image-03.jpg" class="w-full max-w-full max-h-full m-auto" /></li>
+            <li><img src="https://Tailwindmix.b-cdn.net/carousel/carousel-image-04.jpg" class="w-full max-w-full max-h-full m-auto" /></li>
+            <li><img src="https://Tailwindmix.b-cdn.net/carousel/carousel-image-05.jpg" class="w-full max-w-full max-h-full m-auto" /></li>
+        </ul>
+    </div>
+    <!-- Controls -->
+    <div class="absolute left-0 flex items-center justify-between w-full h-0 px-4 top-1/2 " data-glide-el="controls">
+        <button class="inline-flex items-center justify-center w-8 h-8 transition duration-300 border rounded-full lg:w-12 lg:h-12 text-slate-700 border-slate-700 hover:text-slate-900 hover:border-slate-900 focus-visible:outline-none bg-white/20" data-glide-dir="<" aria-label="prev slide">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                <title>prev slide</title>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            </svg>
+        </button>
+        <button class="inline-flex items-center justify-center w-8 h-8 transition duration-300 border rounded-full lg:w-12 lg:h-12 text-slate-700 border-slate-700 hover:text-slate-900 hover:border-slate-900 focus-visible:outline-none bg-white/20" data-glide-dir=">" aria-label="next slide">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                <title>next slide</title>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+            </svg>
+        </button>
+    </div>
+</div>
+
+
+
